eda0757934a3069351fa4378619a196e53755e2a
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-02-22 18:34:05 -0500

    Ejemplo Iterable agregado

---
+++ src/test/java/com/dangwebs/webflux/reactor/WebfluxcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dangwebs.webflux.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
