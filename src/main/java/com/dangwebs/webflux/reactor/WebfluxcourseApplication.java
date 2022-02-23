44a2b282803228724791d999db1fe223a9ba77ba
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-02-22 22:50:23 -0500

    ejemploToString added

--- src/main/java/com/dangwebs/webflux/reactor/WebfluxcourseApplication.java
+++ src/main/java/com/dangwebs/webflux/reactor/WebfluxcourseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,41 +23,9 @@ public class WebfluxcourseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        List<String> usuariosList = new ArrayList<>();
-        usuariosList.add("Andres Guzman");
-        usuariosList.add("Pedro Fulano");
-        usuariosList.add("Maria Fulana");
-        usuariosList.add("Diego Sultano");
-        usuariosList.add("Juan Mengano");
-        usuariosList.add("Bruce Lee");
-        usuariosList.add("Bruce Willis");
-
-        Flux<String> nombres = Flux.fromIterable(usuariosList); // Flux.just("Andres Guzman", "Pedro Fulano", "Maria Fulana", "Diego Sultano", "Juan Mengano", "Bruce Lee", "Bruce Willis");
-
-        Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
-                .filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce"))
-                .doOnNext(usuario -> {
-                    if (usuario == null) {
-                        throw new RuntimeException("Los nombres no pueden ser vacíos");
-                    }
-                    System.out.println(usuario.getNombre() + " " + usuario.getApellido());
-                })
-                .map(usuario -> {
-                    String nombre = usuario.getNombre().toLowerCase();
-                    usuario.setNombre(nombre);
-                    return usuario;
-                });
-
-        usuarios.subscribe(e -> log.info(e.toString()),
-                error -> log.error(error.getMessage()),
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        log.info("Ha finalizado la ejecución del observable con éxito!");
-                    }
-                }
-        );
+        // ejemploItarable();
+        // ejemploFlatMap();
+        ejemploToString();
     }
 
     public void ejemploItarable() throws Exception {
@@ -95,5 +64,53 @@ public class WebfluxcourseApplication implements CommandLineRunner {
                     }
                 }
         );
+    }
+
+    public void ejemploFlatMap() throws Exception {
+
+        List<String> usuariosList = new ArrayList<>();
+        usuariosList.add("Andres Guzman");
+        usuariosList.add("Pedro Fulano");
+        usuariosList.add("Maria Fulana");
+        usuariosList.add("Diego Sultano");
+        usuariosList.add("Juan Mengano");
+        usuariosList.add("Bruce Lee");
+        usuariosList.add("Bruce Willis");
+
+        Flux.fromIterable(usuariosList)
+                .map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+                .flatMap(usuario -> {
+                    if (usuario.getNombre().equalsIgnoreCase("bruce")) {
+                        return Mono.just(usuario);
+                    } return Mono.empty();
+                })
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                })
+                .subscribe(u -> log.info(u.toString()));
+    }
+
+    public void ejemploToString() throws Exception {
+
+        List<Usuario> usuariosList = new ArrayList<>();
+        usuariosList.add(new Usuario("Andres", "Guzman"));
+        usuariosList.add(new Usuario("Pedro", "Fulano"));
+        usuariosList.add(new Usuario("Maria", "Fulana"));
+        usuariosList.add(new Usuario("Diego", "Sultano"));
+        usuariosList.add(new Usuario("Juan", "Mengano"));
+        usuariosList.add(new Usuario("Bruce", "Lee"));
+        usuariosList.add(new Usuario("Bruce", "Willis"));
+
+        Flux.fromIterable(usuariosList)
+                .map(usuario -> usuario.getNombre().toUpperCase().concat(" ").concat(usuario.getApellido().toUpperCase()))
+                .flatMap(nombre -> {
+                    if (nombre.contains("bruce".toUpperCase())) {
+                        return Mono.just(nombre);
+                    } return Mono.empty();
+                })
+                .map(nombre -> nombre.toUpperCase())
+                .subscribe(u -> log.info(u.toString()));
     }
 }
